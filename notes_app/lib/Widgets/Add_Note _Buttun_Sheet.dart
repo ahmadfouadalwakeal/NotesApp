8736fea9658f76490b9fb25c4e4a32ba5b5b51fe
ahmadfouadalwakeal <ahmedfouad2592001@gmail.com>
2024-02-11 17:27:45 +0200

    add field to bottom sheet

--- notes_app/lib/Widgets/Add_Note _Buttun_Sheet.dart
+++ notes_app/lib/Widgets/Add_Note _Buttun_Sheet.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/Widgets/custom_text_field.dart';
+import 'package:notes_app/constants.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: 32,),
-        customtextfield(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          SizedBox(height: 32,),
+          customtextfield(hint: 'Title',),
+          SizedBox(height: 16,),
+          customtextfield(hint: 'Content',maxlines: 5,),
+          SizedBox(height: 50,),
+
+
+        ],
+      ),
     );
   }
 }
--- notes_app/lib/Widgets/custom_text_field.dart
+++ notes_app/lib/Widgets/custom_text_field.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class customtextfield extends StatelessWidget {
-  const customtextfield({super.key});
-
+  const customtextfield({super.key, required this.hint, this.maxlines=1});
+  final String hint;
+  final int maxlines;
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: kPrimaryColor,
+      maxLines: maxlines,
       decoration: InputDecoration(
-        hintText: 'Title',
+        hintText: hint,
         border:buildBorder(),
         enabledBorder:buildBorder(),
         focusedBorder: buildBorder(kPrimaryColor),
-
-      ),
-    );
+      ),);
   }
   OutlineInputBorder buildBorder([color]){
     return OutlineInputBorder(
