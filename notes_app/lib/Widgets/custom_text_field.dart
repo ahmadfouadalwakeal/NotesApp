81d0ea1f1783e65919b7425f18aec418d38967b6
ahmadfouadalwakeal <ahmedfouad2592001@gmail.com>
2024-02-15 14:40:05 +0200

    create add note cubit

--- notes_app/lib/Widgets/custom_text_field.dart
+++ notes_app/lib/Widgets/custom_text_field.dart
@@ -2,12 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class customtextfield extends StatelessWidget {
-  const customtextfield({super.key, required this.hint, this.maxlines=1});
+  const customtextfield({super.key, required this.hint, this.maxlines=1, this.onSaved});
   final String hint;
   final int maxlines;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value){
+        if(value?.isEmpty ?? true){
+          return 'field is required';
+        }else{
+          return null;
+        }
+      },
       cursorColor: kPrimaryColor,
       maxLines: maxlines,
       decoration: InputDecoration(
